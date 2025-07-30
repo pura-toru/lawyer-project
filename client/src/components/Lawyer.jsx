89a90830174f62180e392fbf4f431a6f7d8d7af3
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-07-30 12:42:07 +0700

    Update client to match 29-07-2025 backup

---
+++ client/src/components/Lawyer.jsx
@@ -0,0 +1,9 @@
+const Lawyer= () => {
+  return (
+    <header>
+      <h1>Lawyer</h1>
+    </header>
+  );
+};
+
+export default Lawyer;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import SplashScreen from './components/SplashScreen.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SplashScreen />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
-
-
+);
