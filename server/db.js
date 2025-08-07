4b20e96767661ab9fe6c83dc55f05d753dc5dd7e
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-08-07 18:25:26 +0700

    Create fetch api on index.js & refactor db.js

--- server/db.js
+++ server/db.js
@@ -9,17 +9,4 @@ const pool = mariadb.createPool({
   database: process.env.DB_NAME 
 });
 
-async function testDB() {
-  let conn;
-  try{
-    conn = await pool.getConnection();
-    const result = await conn.query('SELECT * FROM lawyers');
-    console.log('Database connected', result);
-  } catch(err) {
-    console.error('Unable to connect:', err);
-  };
-}
-
-testDB();
-
-module.exports =pool;
+module.exports = pool;
