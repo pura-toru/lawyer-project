189bd3bd4bf5de4e4912f67c1cc33e00ef2a20b5
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-08-07 18:48:46 +0700

    Corrects App.jsx route & remove darrel main.jsx bloat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import Home from "./pages/Home.jsx"
 import Lawyer from "./pages/Lawyer.jsx"
 import Splash from "./pages/SplashScreen.jsx"
@@ -8,19 +8,12 @@ import Splash from "./pages/SplashScreen.jsx"
 function App() {
   const [count, setCount] = useState(0);
 
-  // Basic code to connect/test to backend - cnd
-  // useEffect(() => {
-  //   fetch('http://localhost:6969/api/hello')
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  //     .catch(err => console.error('Fetch error:', err));
-  // }, []);
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/lawyer" element={<Lawyer />} />
       </Routes>
     </>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
-import SplashScreen from './components/SplashScreen.jsx';
-import Home from './components/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-        {/* <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} / */}
     </BrowserRouter>
   </StrictMode>,
 );
