2c0fb39f2d815d8944382a0664607bd55f3f18ac
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-08-08 11:33:21 +0700

    Create Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from "./pages/Home.jsx"
-import Lawyer from "./pages/Lawyer.jsx"
+import Register from "./pages/Register.jsx"
 import Splash from "./pages/SplashScreen.jsx"
+import Lawyer from "./pages/Lawyer.jsx"
 import CreateLawyer from "./pages/CreateLawyer.jsx"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Splash />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/lawyer" element={<Lawyer />} />
         <Route path="/lawyer/create" element={<CreateLawyer />} />
