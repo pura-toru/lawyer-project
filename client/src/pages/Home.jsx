9da825603609af9d774d0d87d260b6b1c0f41e40
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-08-14 20:23:56 +0700

    Clean up CSS & Update Home.jsx to arrow function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 // import '../styles/Home.css'
 
-const Home= () => {
+const Home = () => {
   return (
     <div className="home-container">
-      <header className="header">
+      <header className="search-bar">
         <h1>Peler Up</h1>
-        <input type="text" placeholder="Search for lawyers" className="search-bar" />
+        <input type="text" placeholder="Search for lawyers" />
       </header>
       
       <section className="free-consultation">
