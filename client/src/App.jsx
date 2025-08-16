587ca5e55211b6bf60f6953e060c21facba32cbb
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-08-16 11:53:44 +0700

    Merge from drl and add comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,14 @@ import Register from "./pages/Register.jsx"
 import Splash from "./pages/SplashScreen.jsx"
 import Lawyer from "./pages/Lawyer.jsx"
 import CreateLawyer from "./pages/CreateLawyer.jsx"
+import Test from "./pages/TestBackend.jsx" //Ini page buat test backend, gw udh ada contoh kalo bisa lu ikutin aja ntar ada database dia cara jalan sama soalny api endpoint gw samain. Cara run liat di TestBackend.jsx & app.py di server.
 
 function App() {
   const [count, setCount] = useState(0);
-
+  /* Header gw taro diluar <Routes> biar nongol di tiap page, kykny bisa di set biar nongol di page tertentu tp gw blm research */
   return (
     <>
-      <Header />
+      <Header /> 
       <Routes>
         <Route path="/" element={<Splash />} />
         {/* <Route path="*" element={<NoPage />} /> */}
@@ -21,6 +22,7 @@ function App() {
         { <Route path="/home" /*index*/ element={<Home />} /> }
         <Route path="/lawyer" element={<Lawyer />} />
         <Route path="/lawyer/create" element={<CreateLawyer />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </>
   );
