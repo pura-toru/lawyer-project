868b009061114d2397ccf912b327a2fa060927a3
Day07-M <kaymatthew07@gmail.com>
2025-08-20 14:15:34 +0700

    Adjust navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,9 +4,14 @@ import '../styles/Footer.css'
 const Header = () => {
   return(
     <div className="header">
-      <p>LOGO</p> 
-      <button>Sign Up</button>
-      <button>Sign In</button>
+      <div className='logo'>
+        <img src="../../public/images/logo1.png"/>
+        <p>JURISTIQ</p>
+      </div> 
+      <div className='buttons'>  
+        <button>Sign Up</button>
+        <button>Sign In</button>
+      </div> 
     </div>
   )
 }
