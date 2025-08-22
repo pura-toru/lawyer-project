1239bff4dd92378fa56ffa7420e575f919a952d8
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-08-23 01:17:50 +0700

    Format

--- client/src/pages/SplashScreen.jsx
+++ client/src/pages/SplashScreen.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Splash1 from '../../public/images/Splash1.png';
-import '../styles/SplashScreen.css';
 import { Link } from 'react-router-dom';
 import '../App.css'
+import '../styles/SplashScreen.css';
+import Splash1 from '../../public/images/Splash1.png';
 
 // const [count, setCount] = useState(0)
 const quotes = [
