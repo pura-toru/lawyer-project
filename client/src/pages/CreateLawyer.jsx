367a426b98194ee76c278312dd2eb919c7a023b3
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-08-22 20:09:30 +0700

    Async progress

--- client/src/pages/CreateLawyer.jsx
+++ client/src/pages/CreateLawyer.jsx
@@ -2,23 +2,45 @@ import React, { useState, useEffect } from 'react';
 import '../styles/CreateLawyer.css'; 
 
 const CreateLawyer= () => {
+  const addLawyer = () => {
+    const [name, setName] = useState([]);
+    const [location, setLocation] = useState('');
+    const [biography, setBiography] = useState('');
+    const [experience, setExperience] = useState('');
 
-  return (
-    <div className="lawyer-form">
-      <form>
-        <label>Name:
-          <input name="Name" placeholder="Name" />
-        </label>
-        <label>Biography:
-          <input name="Biography" placeholder="Describe yourself" />
-        </label>
-        <label>Location:
-          <input name="Location" placeholder="Where do you practice?" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    const handleSubmit = async (e) => {
+      try{
+        const res = await fetch('http:/localhost:5000/lawyers/post', {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          }
+          body: JSON.stringify({ username, content }),
+        });
+
+        const data = await res.json();
+        if (res.ok){
+          alert("Posted");
+          setName("");
+          setLocation("");
+        } else{
+          alert(data.message || "Failed to post!");
+        }
+      } catch (err){
+        console.error("Error: " err);
+        alert("Failed to send data!")
+      }
+    }
+
+    return(
+      <div className="<form>
+        
+      </form>-container">
+        <input value="name"
+      >
+      </div>
+    )
+  }
 };
 
 export default CreateLawyer;
